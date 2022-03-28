2d7fcafbdb2cc6a18afe9634e42c47db0ad7ac9c
Julian Kasimir <julian.kasimir@lmwa.de>
2022-03-28 20:08:20 +0200

    Deleted users are now output textually

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,4 +1,6 @@
 <template>
-  <h1><i18n-t keypath="home" /></h1>
-  Version: v0.01
+  <h1>workfield</h1>
+  <!--<i18n-t keypath="home" />-->
+  <!--<p>Url: https://github.com/jolution/workfield</p>-->
+  <p>Version: v0.01</p>
 </template>
